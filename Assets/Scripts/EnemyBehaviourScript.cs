79289adc2c9f4a3fd4813a01d5283a10b32c45d1
Xientra <38855570+Xientra@users.noreply.github.com>
2018-07-31 19:58:42 +0200

    Concept for multiAnim Level + straight down, comeInFromSide Animation

--- Assets/Scripts/EnemyBehaviourScript.cs
+++ Assets/Scripts/EnemyBehaviourScript.cs
@@ -6,35 +6,55 @@ public class EnemyBehaviourScript : MonoBehaviour {
 
     public GameObject CoinDrop;
     public GameObject PowerUpDrop;
-    public GameObject EnemyHealthBarRed;
-    public GameObject EnemyHealthBarGreen;
+    private GameObject EnemyHealthBarRed;
+    private GameObject EnemyHealthBarGreen;
+
+    private Animator animator;
 
     public enum EnemyTypes { Standart }
     public EnemyTypes currendEnemyType = EnemyTypes.Standart;
 
-    public bool hasHealthBar = false;
+    public enum AnimationTypes { StraightDown, ComeInFromRight, ComeInFromLeft }
+    public AnimationTypes currendAnimation;
+    public float AnimationStartDelay = 0f;
+    private float AnimationStartDelayTimeStamp;
+    private bool checkForAnimationDelay = true;
 
+    //only Temp
+    public bool hasHealthBar = false;
+    public bool hasAnimation = false;
+
+    /*--------------------Stats--------------------*/
     public float MaxHealth = 100f;
     public float Health = 100f;
     public float CollisionDamage = 50f;
 
-    public Vector3 direction = new Vector3(1, 0);
-
-    private float movementspeed = 0.1f;
+    //public Vector3 direction = new Vector3(1, 0);
+    //private float movementspeed = 0.1f;
 
     //PowerUp Variables
     public static bool noCollisionDamage = false;
 
-
     void Start() {
+        AnimationStartDelayTimeStamp = Time.time + AnimationStartDelay;
 
         if (hasHealthBar) {
             EnemyHealthBarRed = GetComponentsInChildren<Transform>()[1].gameObject;
             EnemyHealthBarGreen = EnemyHealthBarRed.GetComponentsInChildren<Transform>()[1].gameObject;
         }
+
+        if (hasAnimation) {
+            animator = GetComponent<Animator>();
+        }
     }
 
     void Update() {
+        if (checkForAnimationDelay) {
+            if (AnimationStartDelayTimeStamp < Time.time) {
+                startAnimation();
+            }
+        }
+
         if (Health <= 0) {
             StartSelfDestruction(this.gameObject);
         }
@@ -44,7 +64,7 @@ public class EnemyBehaviourScript : MonoBehaviour {
     }
 
     void FixedUpdate() {
-        transform.Translate(direction * movementspeed * Time.deltaTime);
+        //transform.Translate(direction * movementspeed * Time.deltaTime);
     }
 
     void OnTriggerEnter2D(Collider2D collision) {
@@ -62,17 +82,6 @@ public class EnemyBehaviourScript : MonoBehaviour {
         if (collision.gameObject.CompareTag("Explosion")) {
             Health -= collision.gameObject.GetComponent<LaserBulletData>().damage;
         }
-        /*
-        if (collision.gameObject.CompareTag("Projectile")) {
-            if (collision.gameObject.GetComponent<LaserBulletScript>() != null) {
-                Health -= collision.gameObject.GetComponent<LaserBulletScript>().damage;
-            }
-            else {
-                Health -= collision.gameObject.GetComponent<LaserBulletData>().damage;
-            }
-            Destroy(collision.gameObject);
-        }
-        */
     }
 
     void StartSelfDestruction(GameObject toDestroy) {
@@ -83,13 +92,18 @@ public class EnemyBehaviourScript : MonoBehaviour {
         Destroy(toDestroy);
     }
 
-    void changeEnemyType() {
-        switch (currendEnemyType) {
-            case (EnemyTypes.Standart):
-
-
-
+    void startAnimation() {
+        switch (currendAnimation) {
+            case (AnimationTypes.StraightDown):
+                animator.SetBool("StraightDownBool", true);
+                break;
+            case (AnimationTypes.ComeInFromRight):
+                animator.SetBool("ComeInFromRightBool", true);
+                break;
+            case (AnimationTypes.ComeInFromLeft):
+                animator.SetBool("ComeInFromLeftBool", true);
                 break;
         }
+        
     }
 }
