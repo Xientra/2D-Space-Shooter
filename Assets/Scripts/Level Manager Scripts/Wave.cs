0255c93191a037aad88b5693b9a185250a5340af
Xientra <38855570+Xientra@users.noreply.github.com>
2019-02-04 16:53:03 +0100

    Added and implemented custom UI Sprites; Renamed WaveBehaviorScript to Wave; Sorted the Scripts folder;

---
+++ Assets/Scripts/Level Manager Scripts/Wave.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour {
+
+    public float durationUntilDestroy;
+
+    //public bool isActive = true;
+
+    private int PastSecondsSinceStart = 0;
+    private string OriginalObjectName;
+
+    void Start () {
+        OriginalObjectName = this.gameObject.name;
+        StartCoroutine(DisplaySecondsTillDestruction());
+
+        StartCoroutine(destroyAfterDuration(durationUntilDestroy));
+    }
+	
+	void Update () {
+        if (transform.childCount == 0) {
+            StartCoroutine(destroyAfterDuration(1f));
+        }
+
+	}
+
+    IEnumerator destroyAfterDuration(float _duration) {
+        yield return new WaitForSeconds(_duration);
+        //isActive = false;
+
+        //this.transform.parent.transform.gameObject.GetComponent<EndlessLevelControllerScript>().WaveActive = false;
+        //GameObject.FindGameObjectWithTag("Endless Level Controller").GetComponent<WaveControllerScript>().WaveActive = false;
+        
+        //this.gameObject.SetActive(false);
+        Destroy(this.gameObject);
+    }
+
+    IEnumerator DisplaySecondsTillDestruction() {
+        yield return new WaitForSeconds(1);
+        this.gameObject.name = OriginalObjectName + "  -( " + PastSecondsSinceStart.ToString() + "/" + durationUntilDestroy.ToString() + " )-";
+        PastSecondsSinceStart++;
+        StartCoroutine(DisplaySecondsTillDestruction());
+    }
+}
