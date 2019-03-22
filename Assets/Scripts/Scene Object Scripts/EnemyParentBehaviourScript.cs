f264cd8012070899c645a7296d4d06c59434b7c5
Xientra <38855570+Xientra@users.noreply.github.com>
2019-03-22 20:27:38 +0100

    Animations are now mirrorable and scalable by using the scale of the EnemyParent; Added a expender wave;

--- Assets/Scripts/Scene Object Scripts/EnemyParentBehaviourScript.cs
+++ Assets/Scripts/Scene Object Scripts/EnemyParentBehaviourScript.cs
@@ -13,9 +13,17 @@ public class EnemyParentBehaviourScript : MonoBehaviour {
                 if (t.gameObject.CompareTag("HealthBar"))
                     HealthBarObject = t.gameObject;
             }
+
+            HealthBarObject.transform.localScale = new Vector3(HealthBarObject.transform.localScale.x * transform.lossyScale.normalized.x, HealthBarObject.transform.localScale.y * transform.lossyScale.normalized.y, HealthBarObject.transform.localScale.z * transform.lossyScale.normalized.z); //for Mirroring using the scale
+            //HealthBarObject.transform.localScale = transform.lossyScale;
         }
         if (EnemyObject == null) {
-            Debug.LogError("The EnemyParent "+transform.gameObject.name+" has no EnemyObject assinged. It destroyed itself...");
+            Debug.LogError("The EnemyParent " + transform.gameObject.name + " never had an EnemyObject assinged. It destroyed itself...");
+        }
+        else {
+            EnemyObject.transform.localScale = new Vector3(EnemyObject.transform.localScale.x * (1 / transform.lossyScale.x), EnemyObject.transform.localScale.y * 1 / (transform.lossyScale.y), EnemyObject.transform.localScale.z * 1 / (transform.lossyScale.z)); //for Mirroring using the scale
+            //EnemyObject.transform.localScale = new Vector3(EnemyObject.transform.localScale.x * transform.lossyScale.normalized.x, EnemyObject.transform.localScale.y * transform.lossyScale.normalized.y, EnemyObject.transform.localScale.z * transform.lossyScale.normalized.z); //for Mirroring using the scale
+            //EnemyObject.transform.localScale = transform.lossyScale;
         }
     }
 	
