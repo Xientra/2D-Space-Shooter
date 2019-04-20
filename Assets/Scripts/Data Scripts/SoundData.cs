effa2cf8c1459684dbe172583e4674c924622b8f
Xientra <38855570+Xientra@users.noreply.github.com>
2019-04-20 13:56:53 +0200

    Added the second version of the AudioController ... it won't work;
    The only thing I need now are acuall sounds...

---
+++ Assets/Scripts/Data Scripts/SoundData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public class SoundData {
+
+    public AudioClip SoundClip;
+    
+
+    [Range(0,1)]
+    public float volume = 1f;
+    [Range(0, 3)]
+    public float pitch = 1f;
+    
+    public bool loop = false;
+    public bool isMusic = false;
+
+    [HideInInspector]
+    public AudioSource audioSource;
+}
--- Assets/Scripts/Level Manager Scripts/AudioControllerScript.cs
+++ Assets/Scripts/Level Manager Scripts/AudioControllerScript.cs
@@ -7,8 +7,14 @@ public class AudioControllerScript : MonoBehaviour {
 
     public static AudioControllerScript activeInstance;
 
+
+    public SoundData[] soundData;
+
     public AudioClip[] sounds;
-    private AudioSource[] audioSources;
+
+    private List<AudioSource> audioSourcesList = new List<AudioSource>();
+
+    public bool useV2 = false;
 
     void Awake() {
         if (activeInstance == null) {
@@ -20,39 +26,88 @@ public class AudioControllerScript : MonoBehaviour {
             return;
         }
 
-        
+
+        foreach (SoundData sD in soundData) {
+            sD.audioSource = gameObject.AddComponent<AudioSource>();
+            sD.audioSource.clip = sD.SoundClip;
+            sD.audioSource.volume = sD.volume;
+            sD.audioSource.pitch = sD.pitch;
+            sD.audioSource.loop = sD.loop;
+        }
+
+        /*
         audioSources = new AudioSource[sounds.Length];
 
         for (int i = 0; i < sounds.Length; i++) {
             audioSources[i] = gameObject.AddComponent<AudioSource>();
             audioSources[i].clip = sounds[i];
         }
+        */
     }
 
     void Start() {
     }
 
     void Update() {
+
+        #region v2
+        if (useV2 == true) {
+
+            List<AudioSource> ASToRemove = new List<AudioSource>();
+
+            foreach (AudioSource AuSo in audioSourcesList) {
+                if (AuSo != null) {
+                    if (AuSo.isPlaying == false) {
+                        ASToRemove.Add(AuSo);
+                    }
+                }
+                else Debug.LogWarning("There is a null element in the audioSourcesList. That might just be that way idk");
+            }
+
+            foreach (AudioSource AuSoToRemove in ASToRemove) {
+                audioSourcesList.Remove(AuSoToRemove);
+                Destroy(AuSoToRemove);
+            }
+        }
+        #endregion
     }
 
     public void PlaySound(string _soundName) {
         if (GameControllerScript.SoundIsMuted == false) {
-            
-            GetAudioSource(_soundName).Play();
+            if (useV2 == false) {
+                SoundData sd = GetSoundData(_soundName);
+                sd.audioSource.volume = sd.volume;
+                sd.audioSource.pitch = sd.pitch;
+                sd.audioSource.loop = sd.loop;
+                sd.audioSource.Play();
+            }
+            else {
+                #region v2
+                AudioSource audioS = gameObject.AddComponent<AudioSource>();
 
+                AudioClip ac = GetSound(_soundName);
+                if (ac != null) {
+
+                    audioSourcesList.Add(audioS);
+
+                    audioS.clip = ac;
+                    audioS.Play();
+                }
+                #endregion
+            }
         }
     }
 
-    private AudioSource GetAudioSource(string _soundName) {
-        foreach (AudioSource AuSo in audioSources) {
-            if (AuSo.clip.name == _soundName)
-                return AuSo;
+    private SoundData GetSoundData(string _soundName) {
+        foreach (SoundData sD in soundData) {
+            if (sD.SoundClip.name == _soundName)
+                return sD;
         }
-        Debug.LogError("There is no AudioSource for the Sound with the Name: \"" + _soundName + "\".");
+        Debug.LogError("There is no SoundData for the Sound with the Name: \"" + _soundName + "\".");
         return null;
     }
 
-    //not used anywhere 
+    //only used in v2
     private AudioClip GetSound(string _soundName) {
         foreach (AudioClip ac in sounds) {
             if (ac.name == _soundName)
